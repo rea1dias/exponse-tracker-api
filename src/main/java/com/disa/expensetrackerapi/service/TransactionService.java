4b5318d1ae41071573846273593ea38403ce0275
diasrakhman <esbolkuandik@gmail.com>
2026-05-08 19:46:35 +0500

    get transaction by parametres

--- src/main/java/com/disa/expensetrackerapi/service/TransactionService.java
+++ src/main/java/com/disa/expensetrackerapi/service/TransactionService.java
@@ -2,7 +2,9 @@ package com.disa.expensetrackerapi.service;
 
 import com.disa.expensetrackerapi.domain.dto.transaction.TransactionRequest;
 import com.disa.expensetrackerapi.domain.dto.transaction.TransactionResponse;
+import com.disa.expensetrackerapi.enums.CategoryType;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionService  {
@@ -10,4 +12,12 @@ public interface TransactionService  {
     TransactionResponse createTransaction(TransactionRequest request, Long categoryId);
 
     List<TransactionResponse> getAllTransactions();
+
+    TransactionResponse getTransaction(Long transactionId);
+
+    void deleteTransaction(Long transactionId);
+
+    List<TransactionResponse> getTransactions(CategoryType type, Long categoryId, LocalDate from, LocalDate to);
+
+
 }
